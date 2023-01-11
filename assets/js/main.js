555c9322cbafcd77742d25d1f9e7f347122ac3f6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-01-10 23:13:39 -0300

    dados do formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,8 @@ const form = document.querySelector('[data-formulario="novoItem"]')
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault()
-    console.log('Funcionou!')
+
+    console.log(evento)
+    console.log(evento.target.elements['nome'].value)
+    console.log(evento.target.elements['quantidade'].value)
 })
