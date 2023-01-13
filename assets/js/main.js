4563ef56c8c0eeb0acd829e0635f9d256538bd00
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-01-13 19:24:04 -0300

    adiciona itens na lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,31 @@ form.addEventListener('submit', (evento) => {
     // Cancela o evento
     evento.preventDefault()
 
-    console.log(evento)
-
     // Seleciona o elemento dentro do formulario com o id ou name = "nome" 
-    console.log(evento.target.elements['nome'].value)
-    
+    const nome = evento.target.elements['nome'].value
     // Seleciona o elemento dentro do formulario com o id ou name = "quantidade" 
-    console.log(evento.target.elements['quantidade'].value)
+    const quantidade = evento.target.elements['quantidade'].value
+
+    criaElemento(nome, quantidade)
 })
+
+
+// Seleciona a lista
+const lista = document.querySelector('[data-lista]')
+
+function criaElemento(nomeValor, quantidadeValor) {
+    
+    // <li class="lista__item"><strong class="lista__unidade">7</strong>Camisas</li>
+    
+    const item = document.createElement('li')
+    item.classList.add('lista__item')
+    
+    const quantidade = document.createElement('strong')
+    quantidade.classList.add('lista__quantidade')
+    quantidade.innerHTML = quantidadeValor
+    item.type = hide
+    item.appendChild(quantidade)
+    item.innerHTML += nomeValor
+        
+    lista.appendChild(item)
+}
