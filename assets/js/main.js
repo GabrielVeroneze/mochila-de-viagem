31b9fab907e46c9ada928c8a89c1adefb00338e6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-01-12 21:32:14 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
+// Seleciona o formulário
 const form = document.querySelector('[data-formulario="novoItem"]')
 
+// O evento é acionado quando o formulário for enviado
 form.addEventListener('submit', (evento) => {
+
+    // Cancela o evento
     evento.preventDefault()
 
     console.log(evento)
+
+    // Seleciona o elemento dentro do formulario com o id ou name = "nome" 
     console.log(evento.target.elements['nome'].value)
+    
+    // Seleciona o elemento dentro do formulario com o id ou name = "quantidade" 
     console.log(evento.target.elements['quantidade'].value)
 })
