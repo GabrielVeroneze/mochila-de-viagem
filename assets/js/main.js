117e3e46fda3c98213f4819ec8b6e92231583bba
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-01-17 18:30:55 -0300

    consulta dados do localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,58 @@
 // Seleciona o formulário
-const form = document.querySelector('[data-formulario="novoItem"]')
+const form = document.querySelector('[data-formulario="novoItem"]');
 
 // O evento é acionado quando o formulário for enviado
-form.addEventListener('submit', (evento) => {
+form.addEventListener("submit", (evento) => {
+  // Cancela o evento
+  evento.preventDefault();
 
-    // Cancela o evento
-    evento.preventDefault()
+  // Seleciona o elemento dentro do formulario com o id ou name = "nome" e "quantidade"
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
 
-    // Seleciona o elemento dentro do formulario com o id ou name = "nome" e "quantidade"
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+  criaElemento(nome.value, quantidade.value);
 
-    criaElemento(nome.value, quantidade.value)
-
-    // Apaga o valor dos input
-    nome.value = ''
-    quantidade.value = ''
-})
-
+  // Apaga o valor dos input
+  nome.value = "";
+  quantidade.value = "";
+});
 
 // Seleciona a lista
-const lista = document.querySelector('[data-lista]')
+const lista = document.querySelector("[data-lista]");
 
-// Array de objetos com os dados do item  
-const listaDados = []
+// Array de objetos com os dados do item
+const listaDados = JSON.parse(localStorage.getItem("item")) || [];
+
+listaDados.forEach((dados) => {
+  criaElemento(dados.nome, dados.quantidade);
+});
 
 // Recebe o valor dos input para criar e adicionar os items na lista
 function criaElemento(nomeValor, quantidadeValor) {
-    
-    const item = document.createElement('li')
-    item.classList.add('lista__item')
-    
-    const quantidade = document.createElement('strong')
-    quantidade.classList.add('lista__quantidade')
-    quantidade.innerHTML = quantidadeValor
-    
-    item.appendChild(quantidade)
-    item.innerHTML += nomeValor
+  const item = document.createElement("li");
+  item.classList.add("lista__item");
 
-    lista.appendChild(item)
+  const quantidade = document.createElement("strong");
+  quantidade.classList.add("lista__quantidade");
+  quantidade.innerHTML = quantidadeValor;
 
+  item.appendChild(quantidade);
+  item.innerHTML += nomeValor;
 
-    // Objeto com o nome e quantidade dos items
-    const itemDados = {
-        'nome': nomeValor,
-        'quantidade': quantidadeValor
-    }
+  lista.appendChild(item);
 
-    // Adiciona o objeto itemDados no array listaDados
-    listaDados.push(itemDados)
+  // Objeto com o nome e quantidade dos items
+  const itemDados = {
+    nome: nomeValor,
+    quantidade: quantidadeValor,
+  };
 
-    // Armazena a chave 'item' e o valor 'listaDados' (array de objetos convertido em uma string JSON)
-    localStorage.setItem('item', JSON.stringify(listaDados))
+  // Adiciona o objeto itemDados no array listaDados
+  listaDados.push(itemDados);
+  console.log("🚀 ~ file: main.js:56 ~ criaElemento ~ itemDados", itemDados);
+
+  // Armazena a chave 'item' e o valor 'listaDados' (array de objetos convertido em uma string JSON)
+  localStorage.setItem("item", JSON.stringify(listaDados));
 }
+
+const a   =   3
